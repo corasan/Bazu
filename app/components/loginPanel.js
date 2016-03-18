1e6a01d87fbc3f1a2301b69857988f09cb87023d
corasan <henrypl360@gmail.com>
2016-03-17 22:10:31 -0400

    added styles css and changed the login panel

--- app/components/loginPanel.js
+++ app/components/loginPanel.js
@@ -4,13 +4,14 @@ import { Panel, Input, Col, Button } from 'react-bootstrap'
 export default class LoginPanel extends Component{
     render() {
         return(
-            <Panel>
+            <Panel id="login-panel">
                 <form>
+                    <h3 id="login-title">Login</h3>
                     <Col md={2}/>
                     <Col md={8}>
                         <Input type="email" placeholder="Enter email" />
                         <Input type="password" placeholder="Password" />
-                        <Button bsStyle="primary">Login</Button>
+                        <Button bsStyle="primary" id="login-btn">Login</Button>
                     </Col>
                 </form>
             </Panel>
--- app/pages/Login.js
+++ app/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Panel, Input, Col, Row, Grid } from 'react-bootstrap'
-import LoginPanel from '../components/LoginPanel'
+import LoginPanel from '../components/loginPanel'
 
 export default class App extends Component{
     render() {
