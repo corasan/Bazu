0ddfcff5927317d9c5edfc270dba73bcb0d7ef62
corasan <henrypl360@gmail.com>
2016-03-17 22:41:38 -0400

    login onchange handlers added

--- app/components/loginPanel.js
+++ app/components/loginPanel.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import { Panel, Input, Col, Button } from 'react-bootstrap'
 
 export default class LoginPanel extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleEmail = (e) => {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePassword = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
     render() {
         return(
             <Panel id="login-panel">
@@ -9,8 +25,8 @@ export default class LoginPanel extends Component{
                     <h3 id="login-title">Login</h3>
                     <Col md={2}/>
                     <Col md={8}>
-                        <Input type="email" placeholder="Enter email" />
-                        <Input type="password" placeholder="Password" />
+                        <Input type="email" placeholder="Email" onChange={this.handleEmail} />
+                        <Input type="password" placeholder="Password" onChange={this.handlePassword} />
                         <Button bsStyle="primary" id="login-btn">Login</Button>
                     </Col>
                 </form>
