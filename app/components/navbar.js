e15f6bb09c3e987907b1af3992f74492025066fa
corasan <henrypl360@gmail.com>
2016-03-21 22:52:51 -0400

    changed the navbar design

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react'
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 import { Link } from 'react-router'
 const ref = new Firebase('https://sms-react.firebaseio.com/');
+import NewMessage from './newMessage'
+
 
 export default class NavBar extends Component{
-    logout = () => {
-        ref.unauth();
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: {}
+        }
+    }
+
+    componentWillMount() {
+        let user = ref.getAuth();
+        ref.child('contacts').child(user.uid).on('value', function(data) {
+            this.setState({contacts: data.val()});
+        }.bind(this));
     }
 
     render() {
@@ -14,16 +26,13 @@ export default class NavBar extends Component{
             <Navbar fixedTop>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <Link to="/">SMS App</Link>
+                        <Link to="/">Bazu</Link>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-                        <NavDropdown eventKey={1} title={user.password.email} id="basic-nav-dropdown">
-                            <li><Link eventKey={1.1} to="messages">Historial de mensajes</Link></li>
-                        </NavDropdown>
-                        <li><Link to="login" onClick={this.logout}>Log out</Link></li>
+                        <NewMessage contacts={this.state.contacts} />
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
--- app/components/sidenav.js
+++ app/components/sidenav.js
@@ -2,31 +2,15 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem, Grid, Row, Col, Panel } from 'react-bootstrap'
 import { Link } from 'react-router'
 const ref = new Firebase('https://sms-react.firebaseio.com/');
-import NewMessage from './newMessage'
 import AddContact from './addContact'
 
 export default class SideNav extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: {}
-        }
-    }
-
-    componentWillMount() {
-        let user = ref.getAuth();
-        ref.child('contacts').child(user.uid).on('value', function(data) {
-            this.setState({contacts: data.val()});
-        }.bind(this));
-    }
-
     render() {
         return(
             <div className="sidenav">
                 <Col md={3}>
                     <Panel>
                         <AddContact />
-                        <NewMessage contacts={this.state.contacts} />
                     </Panel>
                 </Col>
             </div>
