6e9e9a093b70672b8ce13a67d92fb63f73c139ee
corasan <henrypl360@gmail.com>
2016-05-13 15:34:21 -0400

    twilio content-type

--- server.js
+++ server.js
@@ -45,7 +45,8 @@ app.post('/upload', upload.single('imageFile'), function (req, res, next) {
                 from: twilioNumber,
                 to: '1'+data[i].number,
                 body: req.body.message,
-                mediaUrl: `https://bazu-app.herokuapp.com/dist/uploads/${file}`
+                mediaUrl: `https://bazu-app.herokuapp.com/dist/uploads/${file}`,
+                mediaContentType: 'image/png'
             }, function(err, message) {
                 if(err) {
                     console.log('Error!', err);
