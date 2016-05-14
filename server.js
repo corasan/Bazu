b010fdeb5dec35735e84c80a4be97cf3932a6659
corasan <henrypl360@gmail.com>
2016-05-14 07:29:37 -0400

    changed back

--- server.js
+++ server.js
@@ -37,34 +37,28 @@ app.post('/upload', upload.single('imageFile'), function (req, res, next) {
     var user = ref.getAuth();
     var file = req.file.filename;
     console.log(req.file.mimetype);
-    // .then(function() {
-    //     res.end();
-    // });
-    return file;
-})
-.then(function(file) {
-    app.post('/send'), function(req, res) {
-        ref.child('contacts').child(req.body.userID).once('value').then(function(dataSnapshot) {
-            res.set('Content-Type', 'image/png');
-            var data = dataSnapshot.val();
-            for(var i in data) {
-                client.messages.create({
-                    from: twilioNumber,
-                    to: '1'+data[i].number,
-                    // body: req.body.message,
-                    mediaContentType: 'image/png',
-                    mediaUrl: `https://bazu-app.herokuapp.com/dist/uploads/${file}`
-                    // mediaUrl: `http://localhost:3000/dist/uploads/${file}`
-                }, function(err, message) {
-                    if(err) {
-                        console.log('Error!', err);
-                    } else {
-                        console.log('Message SID:', message.sid);
-                    }
-                });
-            }
-        });
-    }
+    ref.child('contacts').child(req.body.userID).once('value').then(function(dataSnapshot) {
+        res.set('Content-Type', 'image/png');
+        var data = dataSnapshot.val();
+        for(var i in data) {
+            client.messages.create({
+                from: twilioNumber,
+                to: '1'+data[i].number,
+                body: req.body.message,
+                mediaContentType: 'image/png',
+                // mediaUrl: `https://bazu-app.herokuapp.com/dist/uploads/${file}`
+                mediaUrl: `http://localhost:3000/dist/uploads/${file}`
+            }, function(err, message) {
+                if(err) {
+                    console.log('Error!', err);
+                } else {
+                    console.log('Message SID:', message.sid);
+                }
+            });
+        }
+    }).then(function() {
+        res.end();
+    });
 });
 
 
