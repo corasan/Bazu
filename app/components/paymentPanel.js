2712109f1ff58e9e991ee3fb67314bc1dfb6da52
corasan <henrypl360@gmail.com>
2016-05-21 17:43:16 -0400

    short year

--- app/components/paymentPanel.js
+++ app/components/paymentPanel.js
@@ -18,10 +18,10 @@ const months = [
 ]
 
 const years = [
-    <option value="2016">2016</option>,
-    <option value="2017">2017</option>,
-    <option value="2018">2018</option>,
-    <option value="2019">2019</option>
+    <option value="16">16</option>,
+    <option value="17">17</option>,
+    <option value="18">18</option>,
+    <option value="19">19</option>
 ]
 
 export default class PaymentPanel extends Component {
